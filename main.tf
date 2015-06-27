862eedb11190b224df5ec8c76748d283c034ae5f
Julian Schneider <julian.schneider@zweitag.de>
2015-06-27 15:14:31 +0200

    Update example

--- main.tf
+++ main.tf
@@ -19,9 +19,8 @@ module "test" {
   # ip_addresses = "1.2.3.8,1.2.3.9"
   # => works as excpected
 
-  # ip_addresses = "${docker_container.ubuntu.ip_addresses}"
-  # => fails with "failed to render test.tpl: 1:3: unknown variable accessed: param1" (NOT param2, which contains the ip address)
-
-  # ip_addresses = "${join(",",docker_container.ubuntu.*.ip_address)}"
-  # => fails with "Resource 'docker_container.ubuntu' does not have attribute 'ip_address' for variable 'docker_container.ubuntu.*.ip_address'"
+  ip_addresses = "${join(",",docker_container.ubuntu.*.ip_address)}"
+  # => works as expected for "terraform apply", but fails with
+  # "Resource 'docker_container.ubuntu' does not have attribute 'ip_address' for variable 'docker_container.ubuntu.*.ip_address'"
+  # on "terraform destroy"
 }
